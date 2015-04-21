150855a9f55daff01469c1bff11384b8ce95df52
qingniao99 <huangjianqiang126@163.com>
2015-04-21 15:00:47 +0800

    first

---
+++ preload.js
@@ -0,0 +1,32 @@
+/*
+    sources = {    
+        hashiqi: "https://dn-coding-net-production-static.qbox.me/8628eaef-1a80-420d-98a3-7982c84055ea.jpg"  }
+    temporary:a default value that is live;
+*/
+function preloadImages(sources,callback,temporary){    
+    var loadedImages = 0;    
+    var totalImages = 0; 
+    var images = {};
+    for (var src in sources) {    
+        totalImages++;    
+    }    
+    for (var src in sources) {    
+        images[src] = new Image(); 
+        images[src].onload = function(){ 
+            loadedImages++;
+            var percent=parseInt(loadedImages/totalImages*100)+"%";   
+            if (loadedImages == totalImages) { 
+                callback && callback( percent );    
+            }    
+        }; 
+        images[src].onerror = function(){ 
+            loadedImages++;
+            temporary && (this.src = temporary);
+            var percent=parseInt(loadedImages/totalImages*100)+"%";
+            if (loadedImages == totalImages) {        
+                callback && callback( percent );      
+            }    
+        }; 
+        images[src].src = sources[src];    
+    }    
+}
