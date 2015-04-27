1d71350ab8007690bbe7b953a006d155c7262003
qingniao99 <huangjianqiang126@163.com>
2015-04-27 08:29:12 +0800

    add callbackpercent

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@
         hashiqi: "https://dn-coding-net-production-static.qbox.me/8628eaef-1a80-420d-98a3-7982c84055ea.jpg"  }
     temporary:a default value that is live;
 */
-function preloadImages(sources,callback,temporary){    
+function preloadImages(sources,callbackPercent,callbackComplete,temporary){    
     var loadedImages = 0;    
     var totalImages = 0; 
     var images = {};
@@ -14,18 +14,22 @@ function preloadImages(sources,callback,temporary){
         images[src] = new Image(); 
         images[src].onload = function(){ 
             loadedImages++;
-            var percent=parseInt(loadedImages/totalImages*100)+"%";   
+            //var percent=parseInt(loadedImages/totalImages*100)+"%";
+            var percent=parseInt(loadedImages/totalImages*100)+"%";
+            callbackPercent && callbackPercent(percent);   
             if (loadedImages == totalImages) { 
-                callback && callback( percent );    
+                callbackComplete && callbackComplete( percent );    
             }    
         }; 
         images[src].onerror = function(){ 
             loadedImages++;
             temporary && (this.src = temporary);
+            //var percent=parseInt(loadedImages/totalImages*100)+"%";
             var percent=parseInt(loadedImages/totalImages*100)+"%";
-            if (loadedImages == totalImages) {        
-                callback && callback( percent );      
-            }    
+            callbackPercent && callbackPercent(percent);   
+            if (loadedImages == totalImages) { 
+                callbackComplete && callbackComplete( percent );    
+            }   
         }; 
         images[src].src = sources[src];    
     }    
